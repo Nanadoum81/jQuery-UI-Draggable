b859c3528f9b1cc0c3a2a901424c90a691b4168c
Emmanuel Nanadoum <nanadoum.emmanuel@gmail.com>
2017-06-15 03:29:11 -0400

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// script.js
+
+$(function () {
+    $('.box').draggable();
+    $('#box1').draggable({
+        scroll: true
+    });
+
+    $('#box2').draggable({
+        axis: "x"
+    });
+
+    $('#box3').draggable({
+        axis: "y"
+    });
+
+    $('#box4').draggable({
+        containment: ".container"
+    });
+
+});
